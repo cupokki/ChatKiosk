ba67b76b5cff3f17306a84cd9d89b5e65af39ed3
cupokki <nd1346@gmail.com>
2023-04-11 02:34:33 +0900

    comment

--- services/chat.js
+++ services/chat.js
@@ -18,7 +18,7 @@ class Chat {
     this.menual = menual || `사용자가 음식을 주문하면 "(order:menu) prompt content"의 형태로 대답한다.`;
     this.messages = [
       { role: "system", content: "당신은 세계최고의 점원입니다. 주어지는 메뉴얼 대로 행동하여 고객을 응대합니다." },
-      { role: "user", content: "당신은 세계최고의 점원입니다. 주어지는 메뉴얼 대로 행동하여 고객을 응대합니다. 메뉴얼은 다음과 같습니다. \n" + menual + "이제 고객이 왔습니다. 대답하세요" }
+      { role: "user", content: "당신은 세계최고의 점원입니다. 주어지는 메뉴얼 대로 행동하여 고객을 응대합니다. 메뉴얼은 다음과 같습니다. \n" + this.menual + "이제 고객이 왔습니다. 대답하세요" }
     ];
 
   }
@@ -37,12 +37,15 @@ class Chat {
 
   async question(msg) {
     let parsed = JSON.parse(
-      `{role:"user", content:${msg}}`
+      `{"role":"user", "content":"${msg.toString()}"}`
     )
     this.messages.push(parsed)
     // return answer;
   }
 
 }
+//https://yscho03.tistory.com/m/284
+//문자열로 content를 전달하지말고 id로 content를 대체하여 토근 절약이 가능하다
+//대화 내용을 반영할 유일한 방법인 듯
 // module.exports = { Chat, getCompletion };
 module.exports = { Chat };
