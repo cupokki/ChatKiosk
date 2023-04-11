fb01d321dc163edb384d79253614b92e0af5d872
Jung <nd1346@gmail.com>
2023-04-11 16:11:59 +0900

    how to return msg

--- routers/completion.js
+++ routers/completion.js
@@ -3,20 +3,24 @@ const router = express.Router();
 
 const { Chat } = require("../services/chat.js");
 
-
 router.post("/", async (req, res) => {
   try {
     const { message } = req.body;
     console.log(message)
 
+
+    //보안성 검토
+    req.session.chat = chat;
+
     const chat = new Chat();
+
+    //chat 객체는 세션마다 존재
     chat.create()
-      .then(() => {
-        res.json(chat.completion.data.choices.message);
-        console.log(chat.completion.data.choices);
-        chat.question(message).then(
-          console.log(chat.messages)
-        )
+      .then((result) => {
+        // let data = chat.messages[chat.masseages.length - 1];
+        console.log(result);
+        console.log(chat.messages);
+        res.json(result);
       })
       .catch((err) => {
         console.error(err);
@@ -27,16 +31,6 @@ router.post("/", async (req, res) => {
     console.error(err);
     res.status(500).send("Internal server error");
   }
-  question(message).then(
-    console.log(chat.messages)
-  )
-
-  // while(True){
-  //   chat.say(){
-
-  //   }
-  // }
-  
 
 });
 
--- services/chat.js
+++ services/chat.js
@@ -13,7 +13,6 @@ let menual = ""
 
 class Chat {
   constructor(menu, menual) {
-    this.completion = null;
     this.menu = menu || '불고기버거:{가격:3000원, 재료:{4호번스, 마요네즈, 양파, 레터스, 불고기버거소스, 불고기패티}, 제품소개:"불고기 맛"}';
     this.menual = menual || `사용자가 음식을 주문하면 "(order:menu) prompt content"의 형태로 대답한다.`;
     this.messages = [
@@ -25,11 +24,18 @@ class Chat {
 
   async create() {
     try {
-      this.completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo", //파인튜닝이 안된다 -> createCompletion(text davichi 모델) -> 비싸고 느려
         max_tokens: 300,
         messages: this.messages
       });
+      //get Id
+      let parsedId = JSON.parse(`{"role":"assistant", "content":"${completion.data.id}"}`)
+      this.messages.push(parsedId);
+      //생성한 즉시 메시지 배열에 바로 push하고 메시지만 밖에서 읽게하면 되겠네?
+      return new Promise((resolve, reject)=>{
+          resolve(completion.data.choices.message)
+      }) 
     } catch (error) {
       console.error(error);
     }
@@ -47,5 +53,4 @@ class Chat {
 //https://yscho03.tistory.com/m/284
 //문자열로 content를 전달하지말고 id로 content를 대체하여 토근 절약이 가능하다
 //대화 내용을 반영할 유일한 방법인 듯
-// module.exports = { Chat, getCompletion };
 module.exports = { Chat };
