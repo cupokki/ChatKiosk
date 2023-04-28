6d6e83eb57b842b27c7417e4ba3c6fae3bbe4b33
cupokki <nd1346@gmail.com>
2023-04-29 01:17:40 +0900

    비동기 수정

--- routers/completion.js
+++ routers/completion.js
@@ -3,35 +3,41 @@ const router = express.Router();
 
 const { Chat } = require("../services/chat.js");
 
+
+
+
+//첫 요청-> chat생성-> 초기 스크립트 반환해야함
 router.post("/", async (req, res) => {
-  try {
-    const { message } = req.body;
-    console.log(message)
+  // try {
+    // const { message } = req.body;
+    // console.log(message)
 
 
     //보안성 검토
-    req.session.chat = chat;
+    // req.session.chat = chat;
 
     const chat = new Chat();
 
     //chat 객체는 세션마다 존재
-    chat.create()
-      .then((result) => {
-        // let data = chat.messages[chat.masseages.length - 1];
-        console.log(result);
-        console.log(chat.messages);
-        res.json(result);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Internal server error");
-      });
+  //   chat.create()
+  //     .then((result) => {
+  //       // let data = chat.messages[chat.masseages.length - 1];
+  //       console.log(result);
+  //       // console.log(chat.messages);
+  //       // res.json(chat.messages);
+  //       // return 
+  //     })
+  //     .catch((result) => {
+  //       console.error(result);
+  //       res.status(500).send("Internal server error");
+  //     })
     
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal server error");
-  }
-
+  // } catch (err) {
+  //   console.error(err);
+  //   res.status(500).send("Internal server error");
+  chat.create()//.then(data=>{console.log(data)});
+    // console.log(chat.create());
+  // }
 });
 
 module.exports = router;
--- services/chat.js
+++ services/chat.js
@@ -12,6 +12,7 @@ const openai = new OpenAIApi(configuration);
 let menual = ""
 
 class Chat {
+  
   constructor(menu, menual) {
     this.menu = menu || '불고기버거:{가격:3000원, 재료:{4호번스, 마요네즈, 양파, 레터스, 불고기버거소스, 불고기패티}, 제품소개:"불고기 맛"}';
     this.menual = menual || `사용자가 음식을 주문하면 "(order:menu) prompt content"의 형태로 대답한다.`;
@@ -22,24 +23,55 @@ class Chat {
 
   }
 
-  async create() {
-    try {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo", //파인튜닝이 안된다 -> createCompletion(text davichi 모델) -> 비싸고 느려
-        max_tokens: 300,
-        messages: this.messages
-      });
-      //get Id
-      let parsedId = JSON.parse(`{"role":"assistant", "content":"${completion.data.id}"}`)
-      this.messages.push(parsedId);
-      //생성한 즉시 메시지 배열에 바로 push하고 메시지만 밖에서 읽게하면 되겠네?
-      return new Promise((resolve, reject)=>{
-          resolve(completion.data.choices.message)
-      }) 
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // async create() {
+  //   try {
+  //     const completion = await openai.createChatCompletion({
+  //       model: "gpt-3.5-turbo", //파인튜닝이 안된다 -> createCompletion(text davichi 모델) -> 비싸고 느려
+  //       max_tokens: 300,
+  //       messages: this.messages
+  //     });
+  //     //get Id
+      // let parsedId = JSON.parse(`{"role":"assistant", "content":"${completion.data.id}"}`)
+  //     this.messages.push(parsedId);
+  //     //생성한 즉시 메시지 배열에 바로 push하고 메시지만 밖에서 읽게하면 되겠네?
+  //     return new Promise((resolve, reject)=>{
+  //         resolve(completion.data.choices.message)
+  //     }) 
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  //생성자 초기화 로직
+  //db 접속해서 프롬프트 세팅
+  init(){
+    
+  }
+
+  //대화 컴플리션 생성
+  //context string 반환
+  //message 추가
+  async create(){
+    // return new Promise( async (resolve, reject)=>{
+      await openai.createChatCompletion({
+        model : 'gpt-3.5-turbo',
+        max_tokens : 300,
+        messages : this.messages
+      })
+      .then(completion=>{
+        let parsedId = JSON.parse(`{"role":"assistant", "content":"${completion.data.id}"}`)
+        this.messages.push(parsedId);
+        console.log(completion.data.choices[0].message)
+        // return completion.data.choices.message
+        //resolve()
+      })
+      .catch(err=>{
+        // console.log(err)
+        console.log("err")
+      })
+      
+    // })
+  }
 
   async question(msg) {
     let parsed = JSON.parse(
