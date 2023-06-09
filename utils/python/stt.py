d2f26ae3f1a3a92034f49bc828fbdbf14fcb2931
kimasill <80378085+kimasill@users.noreply.github.com>
2023-06-09 21:09:13 +0900

    Update stt.py

--- utils/python/stt.py
+++ utils/python/stt.py
@@ -1,14 +1,27 @@
 import sys
 import base64
+import io
+import os
+from google.cloud import speech
+
+client = speech.SpeechClient()
 
 encoded_voice = sys.argv[1]
 
-# encoded_voice를 Google Cloud Speech API로 string 추출
-
 string = encoded_voice
 
-result = { "string" : string }
+content = string
+audio = speech.RecognitionAudio(content=content)
 
-# 반환부
-# 표준출력으로 반환 됨
-print(result, end='')
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="ko-KR",
+)
+
+response = client.recognize(config = config, audio = audio)
+
+for trans in response.results:
+    result = {trans.alternatives[0].transcript: string}
+    print(result, end='')
