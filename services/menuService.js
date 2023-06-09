57024d4e60f15253ee4a970c96936e5e4337dde1
cupokki <nd1346@gmail.com>
2023-06-09 15:27:52 +0900

    docker-compose

--- services/menuService.js
+++ services/menuService.js
@@ -1,7 +1,14 @@
 const Shop = require("../models/shop")
 
 exports.getMenu = async (req, res) =>{
+    console.log(`test`)
     const shop_id = req.params.shop_id
-    data = await Shop.getMenuList(shop_id)
+    try{
+        data = await Shop.getMenuList(shop_id)
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).json({error:e})
+    }
     res.json(data)
 }
