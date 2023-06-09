dad0dce908ef241182e3a1c8b4972eeae70f37b2
kimasill <80378085+kimasill@users.noreply.github.com>
2023-06-09 22:32:16 +0900

    TestCode Update

--- utils/python/tts.py
+++ utils/python/tts.py
@@ -1,22 +1,23 @@
 import sys
 import base64
 
-string = sys.argv[1] #테스트 시 주석
+#string = sys.argv[1] #테스트 시 주석
 
 def tts() :
 
     from gtts import gTTS
 
     # Test Code
-    # string = "안녕하세요"
+    string = "안녕하세요"
 
     tts = gTTS(string, lang="ko")
     tts.save('trans.base64')
 
     f = open('trans.base64', 'rb')
     file = f.read()
+    data = base64.b64encode(file)
     f.close()
-    result = {"data": file}
+    result = {"data": data}
 
     print(result, end='')
 
