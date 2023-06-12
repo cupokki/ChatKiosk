c48bf679be22d4157a807569ef898f7bb705f95a
kimasill <80378085+kimasill@users.noreply.github.com>
2023-06-12 23:26:28 +0900

    moudle patch

--- utils/python/stt.py
+++ utils/python/stt.py
@@ -3,16 +3,21 @@ import base64
 import io
 import os
 from google.cloud import speech
+from pydub import AudioSegment
 
 RECORDFILE = "../temp/recordFile.mp3"
+TRANSFILE = "../temp/transFile.flac"
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-cloud-sdk/stts-key.json"
 
 def stt() :
 
+    audseg = AudioSegment.from_mp3(RECORDFILE)
+    audseg.export(TRANSFILE, format="wav")
+
     client = speech.SpeechClient()
 
-    file_name = os.path.join(os.path.dirname(__file__), ".", RECORDFILE)
+    file_name = os.path.join(os.path.dirname(__file__), ".", TRANSFILE)
 
     with io.open(file_name, "rb") as audio_file:
         content = audio_file.read()
@@ -20,8 +25,9 @@ def stt() :
 
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
+        sample_rate_hertz=48000,
         language_code="ko-KR",
+        audio_channel_count=2,
     )
 
     response = client.recognize(config = config, audio = audio)
