806e2e8710deae423c537e573507e2e5f3104232
cupokki <nd1346@gmail.com>
2023-06-12 11:44:48 +0900

    d

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ app.use(session({
   saveUninitialized: true, // 세션이 저장되기 전에 uninitialized 상태로 미리 만들어서 저장합니다.
   cookie: {
     maxAge: 1800000, // 30 minutes
-    path : '/',
-    httpOnly : false,
-    sameSite :'none',
-    secure : true,
   },
 }));
 
